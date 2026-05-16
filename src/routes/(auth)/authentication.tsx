31bcd01c6c496acc64e6265e47a391811e8dc335
Felix Pherry <123felixpherry@gmail.com>
2026-05-16 16:20:23 +0700

    chore: fix biome formatting and lint

--- src/routes/(auth)/authentication.tsx
+++ src/routes/(auth)/authentication.tsx
@@ -2,9 +2,9 @@ import { createFileRoute } from "@tanstack/react-router";
 import AuthenticationPage from "#/modules/authentication/authentication-page";
 
 export const Route = createFileRoute("/(auth)/authentication")({
-  component: RouteComponent,
+	component: RouteComponent,
 });
 
 function RouteComponent() {
-  return <AuthenticationPage />;
+	return <AuthenticationPage />;
 }
